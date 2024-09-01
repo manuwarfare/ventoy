12323b19054d323df8be284a69ffbd82df2647a7
manuelguerra <ar.manuelguerra@gmail.com>
2024-09-01 17:22:12 -0400

    Modulate files to to get correct packaging, several changes. Please view commit log to comprend changes

--- build_gtk.sh
+++ build_gtk.sh
@@ -1,45 +1,78 @@
 #!/bin/bash
 
-build_func() {    
-    libsuffix=$2
-    toolDir=$3
-    gtkver=$4   
+set -e
 
-    if [ "$libsuffix" = "aa64" ]; then
-        EXD=./EXLIB/aarch64
-        GTKFLAG="-pthread -I$EXD/usr/include/gtk-3.0 -I$EXD/usr/include/atk-1.0 -I$EXD/usr/include/at-spi2-atk/2.0 -I$EXD/usr/include/pango-1.0 -I$EXD/usr/include/gio-unix-2.0/ -I$EXD/usr/include/cairo -I$EXD/usr/include/gdk-pixbuf-2.0 -I$EXD/usr/include/glib-2.0 -I$EXD/usr/lib64/glib-2.0/include -I$EXD/usr/include/at-spi-2.0 -I$EXD/usr/include/dbus-1.0 -I$EXD/usr/lib64/dbus-1.0/include -I$EXD/usr/include/harfbuzz -I$EXD/usr/include/freetype2 -I$EXD/usr/include/pixman-1 -I$EXD/usr/include/libpng15 -I$EXD/usr/include/libdrm"
-        XXLIB="-Wl,-rpath-link $EXD/usr/lib64 -Wl,-rpath-link $EXD/lib64 -Wno-deprecated-declarations -L$EXD/usr/lib64 -lgtk-3 -lgdk-3 -latk-1.0 -lgio-2.0 -lpangocairo-1.0 -lgdk_pixbuf-2.0 -lcairo-gobject -lpango-1.0 -lcairo -lgobject-2.0 -lglib-2.0 "
-    elif [ "$libsuffix" = "m64e" ]; then
-        EXDI=./EXLIB/mips64el
-        EXDL=./EXLIB/mips64el/usr/lib/mips64el-linux-gnuabi64
-        EXDL2=./EXLIB/mips64el/lib/mips64el-linux-gnuabi64
-        GTKFLAG="-pthread -I$EXDI/usr/include/gtk-3.0 -I$EXDI/usr/include/at-spi2-atk/2.0 -I$EXDI/usr/include/at-spi-2.0 -I$EXDI/usr/include/dbus-1.0 -I./EXLIB/mips64el/usr/lib/x86_64-linux-gnu/dbus-1.0/include -I$EXDI/usr/include/gtk-3.0 -I$EXDI/usr/include/gio-unix-2.0 -I$EXDI/usr/include/cairo -I$EXDI/usr/include/pango-1.0 -I$EXDI/usr/include/harfbuzz -I$EXDI/usr/include/pango-1.0 -I$EXDI/usr/include/fribidi -I$EXDI/usr/include/harfbuzz -I$EXDI/usr/include/atk-1.0 -I$EXDI/usr/include/cairo -I$EXDI/usr/include/pixman-1 -I$EXDI/usr/include/uuid -I$EXDI/usr/include/freetype2 -I$EXDI/usr/include/libpng16 -I$EXDI/usr/include/gdk-pixbuf-2.0 -I$EXDI/usr/include/libmount -I$EXDI/usr/include/blkid -I$EXDI/usr/include/glib-2.0 -I$EXDL/glib-2.0/include"
-        XXLIB="-Wl,-rpath-link $EXDL -Wl,-rpath-link $EXDL2 -Wno-deprecated-declarations -L$EXDL -L$EXDL2 -lm -lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -lharfbuzz -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0 -lpcre "
-    else
-        if [ "$gtkver" = "gtk3" ]; then
-            GTKFLAG=$(pkg-config --cflags --libs gtk+-3.0)
-            GLADE=""
-        else
-            GTKFLAG=$(pkg-config --cflags --libs gtk+-2.0)
-            GLADE=$(pkg-config --cflags --libs libglade-2.0)
-        fi
-        XXLIB=""
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Check for required tools
+for cmd in gcc pkg-config make; do
+    if ! command_exists $cmd; then
+        echo "Error: $cmd is not installed. Please install it and try again."
+        exit 1
     fi
-    
-    XXFLAG="-std=gnu99 -D_FILE_OFFSET_BITS=64 $GTKFLAG $GLADE -Wall"
-    
-    
-    echo "CC=$1 libsuffix=$libsuffix toolDir=$toolDir $gtkver"    
-    
-    $1 $XXFLAG -c -Wall -Wextra -Wshadow -Wformat-security -Winit-self \
+done
+
+# Get the target architecture
+ARCH=${1:-$(dpkg-architecture -qDEB_HOST_ARCH)}
+
+case $ARCH in
+    amd64|x86_64)
+        CC="gcc"
+        LIBSUFFIX="64"
+        TOOLDIR="x86_64"
+        ;;
+    i386)
+        CC="gcc -m32"
+        LIBSUFFIX="32"
+        TOOLDIR="i386"
+        ;;
+    arm64|aarch64)
+        if ! command_exists aarch64-linux-gnu-gcc; then
+            echo "Error: aarch64-linux-gnu-gcc not found. Install gcc-aarch64-linux-gnu package."
+            exit 1
+        fi
+        CC="aarch64-linux-gnu-gcc"
+        LIBSUFFIX="aa64"
+        TOOLDIR="aarch64"
+        ;;
+    mips64el)
+        if ! command_exists mips64el-linux-gnuabi64-gcc; then
+            echo "Error: mips64el-linux-gnuabi64-gcc not found. Install gcc-mips64el-linux-gnuabi64 package."
+            exit 1
+        fi
+        CC="mips64el-linux-gnuabi64-gcc"
+        LIBSUFFIX="m64e"
+        TOOLDIR="mips64el"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+# Function to build Ventoy
+build_ventoy() {
+    local gtkver="gtk3"
+
+    echo "Building for $ARCH using $CC"
+
+    # Get GTK flags
+    GTKFLAGS=$(pkg-config --cflags --libs gtk+-3.0)
+
+    # Build civetweb
+    $CC $GTKFLAGS -c -Wall -Wextra -Wshadow -Wformat-security -Winit-self \
         -Wmissing-prototypes -O2 -DLINUX \
         -I./Ventoy2Disk/Lib/libhttp/include \
         -DNDEBUG -DNO_CGI -DNO_CACHING -DNO_SSL -DSQLITE_DISABLE_LFS -DSSL_ALREADY_INITIALIZED \
         -DUSE_STACK_SIZE=102400 -DNDEBUG -fPIC \
         ./Ventoy2Disk/Lib/libhttp/include/civetweb.c \
         -o ./civetweb.o
-    
-    $1 -O2 -Wall -Wno-unused-function -DSTATIC=static -DINIT= \
+
+    # Build Ventoy2Disk
+    $CC -O2 -Wall -Wno-unused-function -DSTATIC=static -DINIT= \
         -I./Ventoy2Disk \
         -I./Ventoy2Disk/Core \
         -I./Ventoy2Disk/Web \
@@ -50,11 +83,10 @@ build_func() {
         -I./Ventoy2Disk/Lib/xz-embedded/linux/include \
         -I./Ventoy2Disk/Lib/xz-embedded/linux/include/linux \
         -I./Ventoy2Disk/Lib/xz-embedded/userspace \
-        -I ./Ventoy2Disk/Lib/exfat/src/libexfat \
-        -I ./Ventoy2Disk/Lib/exfat/src/mkfs \
-        -I ./Ventoy2Disk/Lib/fat_io_lib \
-        \
-        -L ./Ventoy2Disk/Lib/fat_io_lib/lib \
+        -I./Ventoy2Disk/Lib/exfat/src/libexfat \
+        -I./Ventoy2Disk/Lib/exfat/src/mkfs \
+        -I./Ventoy2Disk/Lib/fat_io_lib \
+        -L./Ventoy2Disk/Lib/fat_io_lib/lib \
         Ventoy2Disk/main_gtk.c \
         Ventoy2Disk/Core/*.c \
         Ventoy2Disk/Web/*.c \
@@ -63,38 +95,34 @@ build_func() {
         Ventoy2Disk/Lib/exfat/src/libexfat/*.c \
         Ventoy2Disk/Lib/exfat/src/mkfs/*.c \
         Ventoy2Disk/Lib/fat_io_lib/*.c \
-        $XXLIB \
         -l pthread \
         ./civetweb.o \
-        -o Ventoy2Disk.${gtkver}_$libsuffix $XXFLAG 
+        -o Ventoy2Disk.${gtkver}_$LIBSUFFIX $GTKFLAGS
 
-    rm -f *.o
-    
-    if [ "$libsuffix" = "aa64" ]; then
-        aarch64-linux-gnu-strip Ventoy2Disk.${gtkver}_$libsuffix
-    elif [ "$libsuffix" = "m64e" ]; then
-        mips-linux-gnu-strip Ventoy2Disk.${gtkver}_$libsuffix
-    else
-        strip Ventoy2Disk.${gtkver}_$libsuffix
+    # Build VentoyGUI
+    $CC -O2 -D_FILE_OFFSET_BITS=64 Ventoy2Disk/ventoy_gui.c Ventoy2Disk/Core/ventoy_json.c \
+        -I Ventoy2Disk/Core -DVTOY_GUI_ARCH="\"$TOOLDIR\"" \
+        -o VentoyGUI.$TOOLDIR $GTKFLAGS
+
+    # Strip binaries
+    strip Ventoy2Disk.${gtkver}_$LIBSUFFIX VentoyGUI.$TOOLDIR
+
+    # Rename and move binaries
+    if [ -e Ventoy2Disk.${gtkver}_$LIBSUFFIX ]; then
+        mv Ventoy2Disk.${gtkver}_$LIBSUFFIX Ventoy2Disk.${gtkver}
+        mkdir -p tool/$TOOLDIR
+        cp Ventoy2Disk.${gtkver} tool/$TOOLDIR/
+        rm Ventoy2Disk.${gtkver}
     fi
-    
-    rm -f ../INSTALL/tool/$toolDir/Ventoy2Disk.${gtkver}_$libsuffix
-    cp -a Ventoy2Disk.${gtkver}_$libsuffix ../INSTALL/tool/$toolDir/Ventoy2Disk.${gtkver}
-    
-    $1 -O2 -D_FILE_OFFSET_BITS=64 Ventoy2Disk/ventoy_gui.c Ventoy2Disk/Core/ventoy_json.c -I Ventoy2Disk/Core  -DVTOY_GUI_ARCH="\"$toolDir\"" -o VentoyGUI.$toolDir
-    cp -a VentoyGUI.$toolDir ../INSTALL/
+
+    # Rename VentoyGUI binary to 'ventoy'
+    if [ -e VentoyGUI.$TOOLDIR ]; then
+        mv VentoyGUI.$TOOLDIR ventoygui
+    fi
+
+    # Clean up
+    echo "Build completed successfully."
 }
 
-
-build_func "gcc" '64' 'x86_64' 'gtk3'
-
-build_func "gcc" '64' 'x86_64' 'gtk2'
-
-build_func "gcc -m32" '32' 'i386' 'gtk2'
-build_func "gcc -m32" '32' 'i386' 'gtk3'
-
-build_func "aarch64-linux-gnu-gcc" 'aa64' 'aarch64' 'gtk3'
-
-export PATH=/opt/mips-loongson-gcc8-linux-gnu-2021-02-08/bin/:$PATH
-build_func "mips-linux-gnu-gcc -mips64r2 -mabi=64" 'm64e' 'mips64el' 'gtk3'
-
+# Main execution
+build_ventoy
